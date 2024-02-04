13230d0d60233afd554fe0161148519f59bb253a
Submarinetwentythousand <154326933+LeaguesUndertheSea@users.noreply.github.com>
2024-02-04 21:13:42 +0800

    Add files via upload
    学

---
+++ hello.c/wisdom.c
@@ -0,0 +1,88 @@
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	printf("Hello World\n");
+//
+//	return 0;
+//}
+//#include<stdio.h>
+
+//int main(void)
+//{
+//	printf("Hello World!\n");
+//
+//	return 0;
+//}
+//#include<stdio.h>
+
+//int main(void)
+//{
+//	printf("Hello World!\n");
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//
+//	printf("Hello World!\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//	printf("Hello World!\n");
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//
+//	printf("Hello World!\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//
+//	printf("Hello World!\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//
+//	printf("Hello World!\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//
+//	printf("Hello World!\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//
+//	printf("Hello World!\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main(void)
+//{
+//
+//	printf("Hello World!\n");
+//	return 0;
+//}
